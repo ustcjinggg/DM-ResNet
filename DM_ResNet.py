5d46f4847b3d83b96c4dfbb68abe5f55fb92fa20
ustcjinggg <38032906+ustcjinggg@users.noreply.github.com>
2021-07-20 09:23:50 +0800

    Update DM_ResNet.py

--- DM_ResNet.py
+++ DM_ResNet.py
@@ -108,32 +108,32 @@ blocks_dict = {
 
 class DM_Module(nn.Module):
 	def __init__(self, in_channel, in_spatial, cha_ratio=1, spa_ratio=1, down_ratio=1):
-		super(DM_Module, self).__init__()
+	    super(DM_Module, self).__init__()
 
-		self.in_channel = in_channel
-		self.in_spatial = in_spatial
+	    self.in_channel = in_channel
+	    self.in_spatial = in_spatial
 		
-		self.inter_channel = in_channel // cha_ratio
-		self.inter_spatial = in_spatial // spa_ratio
+	    self.inter_channel = in_channel // cha_ratio
+	    self.inter_spatial = in_spatial // spa_ratio
 
-		self.sigmoid = nn.Sigmoid()
-        self.is_skip = True
-		self.gx_spatial = nn.Sequential(
+	    self.sigmoid = nn.Sigmoid()
+            self.is_skip = True
+	    self.gx_spatial = nn.Sequential(
             nn.Conv2d(in_channels=self.in_channel, out_channels=self.inter_channel,
                     kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(self.inter_channel),
             nn.ReLU()
-		)
+	    )
 
-		self.gg_spatial = nn.Sequential(
+	    self.gg_spatial = nn.Sequential(
             nn.Conv2d(in_channels=self.in_spatial * 2, out_channels=self.inter_spatial,
                     kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(self.inter_spatial),
             nn.ReLU()
-		)
+	    )
 
-		num_channel_s = 1 + self.inter_spatial
-		self.W_spatial = nn.Sequential(
+	    num_channel_s = 1 + self.inter_spatial
+	    self.W_spatial = nn.Sequential(
             nn.Conv2d(in_channels=num_channel_s, out_channels=num_channel_s//down_ratio,
                     kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(num_channel_s//down_ratio),
@@ -141,47 +141,45 @@ class DM_Module(nn.Module):
             nn.Conv2d(in_channels=num_channel_s//down_ratio, out_channels=1,
                     kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(1)
-		)
+	    )
 
-		self.theta_spatial = nn.Sequential(
+	    self.theta_spatial = nn.Sequential(
             nn.Conv2d(in_channels=self.in_channel, out_channels=self.inter_channel,
                             kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(self.inter_channel),
             nn.ReLU()
-		)
-		self.phi_spatial = nn.Sequential(
+	    )
+	    self.phi_spatial = nn.Sequential(
             nn.Conv2d(in_channels=self.in_channel, out_channels=self.inter_channel,
                         kernel_size=1, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(self.inter_channel),
             nn.ReLU()
-		)
+	    )
 
 				
 	def forward(self, x):
-		b, c, h, w = x.size()
-		#pdb.set_trace()
-        # spatial attention
-		theta_xs = self.theta_spatial(x)	
-		phi_xs = self.phi_spatial(x)
-		theta_xs = theta_xs.view(b, self.inter_channel, -1)
-		theta_xs = theta_xs.permute(0, 2, 1)
-		phi_xs = phi_xs.view(b, self.inter_channel, -1)
-		#pdb.set_trace()
-		Gs = torch.matmul(theta_xs, phi_xs)
-		Gs_in = Gs.permute(0, 2, 1).view(b, h*w, h, w)
-		Gs_out = Gs.view(b, h*w, h, w)
-		Gs_joint = torch.cat((Gs_in, Gs_out), 1)
+	    b, c, h, w = x.size()
+	    theta_xs = self.theta_spatial(x)	
+	    phi_xs = self.phi_spatial(x)
+	    theta_xs = theta_xs.view(b, self.inter_channel, -1)
+	    theta_xs = theta_xs.permute(0, 2, 1)
+	    phi_xs = phi_xs.view(b, self.inter_channel, -1)
+		
+	    Gs = torch.matmul(theta_xs, phi_xs)
+	    Gs_in = Gs.permute(0, 2, 1).view(b, h*w, h, w)
+	    Gs_out = Gs.view(b, h*w, h, w)
+	    Gs_joint = torch.cat((Gs_in, Gs_out), 1)
         
-		Gs_joint = self.gg_spatial(Gs_joint)
+	    Gs_joint = self.gg_spatial(Gs_joint)
     
-		g_xs = self.gx_spatial(x)
-		g_xs = torch.mean(g_xs, dim=1, keepdim=True)
-		ys = torch.cat((g_xs, Gs_joint), 1)
+	    g_xs = self.gx_spatial(x)
+	    g_xs = torch.mean(g_xs, dim=1, keepdim=True)
+	    ys = torch.cat((g_xs, Gs_joint), 1)
         
-		W_ys = self.W_spatial(ys)
+	    W_ys = self.W_spatial(ys)
         
-        out = self.sigmoid(W_ys.expand_as(x)) * x
-		return out
+            out = self.sigmoid(W_ys.expand_as(x)) * x
+	    return out
         
         
 class DM_ResNet(nn.Module):
